4d914caeefda5d49541dba60c04467645f32f8e3
Chintamani Prasad <cmanix07@users.noreply.github.com>
2021-06-07 07:30:55 +0900

    Add files via upload

---
+++ netflix-eureka-naming-server/src/test/java/com/lpras/microservices/netflixeurekanamingserve/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.lpras.microservices.netflixeurekanamingserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
