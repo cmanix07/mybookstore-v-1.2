a1e8ada5d650cebca2cb16cc5d208874c8dfff6f
Chintamani Prasad <cmanix07@users.noreply.github.com>
2021-06-10 22:45:33 +0900

    Add files via upload

---
+++ apringboot-actuator-admin/src/test/java/com/lpras/springboot/StartMyBootAppTests.java
@@ -0,0 +1,13 @@
+package com.lpras.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartMyBootAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
